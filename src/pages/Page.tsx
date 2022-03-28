804dc82fa1d58480c9c27e5ce2f8f68e87d5e3c2
franklindrw <franklindrw@gmail.com>
2022-03-28 18:22:13 -0300

    adicionando imagem

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,22 +1,27 @@
-import { IonContent, IonHeader, IonPage, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonToolbar } from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+
+const picproLogo = require("../img/picpro.png");
+const style = require("./page.module.scss");
 
 const Page: React.FC = () => {
+    return (
+        <IonPage>
+            <IonContent color="primary">
+                <IonHeader class="ion-no-border">
+                    <IonToolbar color="primary">
+                        <img
+                            src={picproLogo}
+                            alt="teste"
+                            className={style.logo}
+                        />
+                    </IonToolbar>
+                </IonHeader>
 
-  return (
-    <IonPage >
-      <IonContent color="primary">
-        <IonHeader class="ion-no-border">
-          <IonToolbar color="primary">
-            <p>teste</p>
-            <img src="../img/picpro.png" alt="" />
-          </IonToolbar>
-        </IonHeader>
-
-        <ExploreContainer name="Hello world" />
-      </IonContent>
-    </IonPage>
-  );
+                <ExploreContainer name="Hello world" />
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Page;
