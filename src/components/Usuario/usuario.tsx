e1a419fcf20c4fd34983cf92bb59a19c5d33757c
franklindrw <franklindrw@gmail.com>
2022-04-02 10:44:13 -0300

    atualização

---
+++ src/components/Usuario/usuario.tsx
@@ -0,0 +1,33 @@
+import { IonAvatar, IonItem, IonLabel } from "@ionic/react";
+import "./usuario.module.scss";
+
+const nome = "franklindrw";
+const presente = require("../../img/presente.png");
+const chat = require("../../img/chat.png");
+
+const Usuario: React.FC = () => {
+    return (
+        <>
+            <IonItem lines="none" color="primary">
+                <IonAvatar>
+                    <img
+                        src="https://github.com/franklindrw.png"
+                        alt="avatar"
+                    />
+                </IonAvatar>
+
+                <IonLabel>
+                    <p>Olá,</p>
+                    <p>@{nome}</p>
+                </IonLabel>
+
+                <IonLabel>
+                    <img src={presente} alt="icone de presente" />
+                    <img src={chat} alt="icone de chat" />
+                </IonLabel>
+            </IonItem>
+        </>
+    );
+};
+
+export default Usuario;
--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,23 +1,25 @@
 import { IonContent, IonHeader, IonPage, IonToolbar } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
+import Usuario from "../components/Usuario/usuario";
+import "./page.module.scss";
 
 const picproLogo = require("../img/picpro.png");
-const style = require("./page.module.scss");
+const kenzieLogo = require("../img/logo-kenzie.png");
 
 const Page: React.FC = () => {
     return (
         <IonPage>
             <IonContent color="primary">
-                <IonHeader class="ion-no-border">
+                <IonHeader class="ion-no-border teste">
                     <IonToolbar color="primary">
-                        <img
-                            src={picproLogo}
-                            alt="teste"
-                            className={style.logo}
-                        />
+                        <div>
+                            <img src={picproLogo} alt="teste" />
+                            <img src={kenzieLogo} alt="teste" />
+                        </div>
                     </IonToolbar>
                 </IonHeader>
 
+                <Usuario />
                 <ExploreContainer name="Hello world" />
             </IonContent>
         </IonPage>
